fdeee267dd16c47d897e1476d88f82b6e49b6325
Teguera <aboubacarteguera21@gmail.com>
2023-06-04 00:33:43 +0200

    fin du projet

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,15 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
+      colors:{
+
+      },
+      screens:{
+
+      },
+      fontFamily:{
+        nunito:'Nunito'
+      }
     },
   },
   plugins: [],
